36719fabea873f900945bf8042438b2b86476a90
Martti Naaber <martnaa@greeny.cs.tlu.ee>
2015-09-07 13:47:10 +0300

    added logic

--- helloworld.php
+++ helloworld.php
@@ -1,7 +1,26 @@
 <?php
 
-$first_name = "Martti"; //muutuja ees peab olema dollarimärk
+	$first_name = "Martti"; //muutuja ees peab olema dollarimärk
+	$last_name = "Naaber";
 
-echo $first_name //echo trükib välja, nagu Java "console.log"
+	echo $first_name." ".$last_name; //"echo" trükib välja, nagu Java "console.log"
+
+
+?>
+
+<br>
+
+<?php
+
+	$age = 10;
+	
+	//loogikatehted >, <, <=, >=, ==, !=(ei võrdu)
+	//if(loogikatehe){tõene} else{väär}
+	
+	if($age < 18){
+		echo "Alaealine";
+	} else {
+		echo "Täisealine";
+	}
 
 ?>
