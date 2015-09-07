99284a521abe2fbc8983e3ac6c9b421bb1cd6775
Martti Naaber <martnaa@greeny.cs.tlu.ee>
2015-09-07 15:42:04 +0300

    logimine

--- user_form.php
+++ user_form.php
@@ -16,6 +16,11 @@
 		}
 		if(empty($_POST["password"])) {
 		$password_error = "Ei saa olla tühi";
+		} else {
+			if(strlen($_POST["password"]) < 8) {
+				$password_error = "Peab olema vähemalt 8 sümbolit pikk";
+			}
+			
 		}
 	}
 ?>
@@ -28,8 +33,8 @@
 		
 		<h2>Login</h2>
 		<form action="user_form.php" method="post">
-			<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?> <br><br>
-			<input name="password" type="password" placeholder="Parool"> <?php echo $password_error; ?> <br><br>
+			<input name="email" type="email" placeholder="E-post">* <?php echo $email_error; ?> <br><br>
+			<input name="password" type="password" placeholder="Parool">* <?php echo $password_error; ?> <br><br>
 		
 			<input type="submit" value="Logi sisse">
 		</form>
