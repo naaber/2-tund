0bfa644fd142f47938e61c6f4ea6cc7f73b064e7
Martti Naaber <martnaa@greeny.cs.tlu.ee>
2015-09-07 15:34:24 +0300

    login

--- user_form.php
+++ user_form.php
@@ -1,5 +1,23 @@
 <?php
 
+	//jutumärkide vahele input elemendi NAME
+	//echo $_POST["email"];
+	//echo $_POST["password"];
+	
+	$email_error = " ";
+	$password_error = " ";
+	
+	//Kontrolli ainult siis, kui kasutaja vajutab "Logi sisse" nuppu
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+		//Kontrollime kasutaja e-posti, et see ei ole tühi
+		if(empty($_POST["email"])) {
+		$email_error = "Ei saa olla tühi";
+		}
+		if(empty($_POST["password"])) {
+		$password_error = "Ei saa olla tühi";
+		}
+	}
 ?>
 
 <html>
@@ -9,11 +27,12 @@
 	<body>
 		
 		<h2>Login</h2>
-		<input type="email" placeholder="E-post"> <br><br>
-		<input type="password" placeholder="Parool"> <br><br>
+		<form action="user_form.php" method="post">
+			<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?> <br><br>
+			<input name="password" type="password" placeholder="Parool"> <?php echo $password_error; ?> <br><br>
 		
-		<input type="submit" value="Logi sisse">
-		
+			<input type="submit" value="Logi sisse">
+		</form>
 		<h2>Create user</h2>
 		
 	</body>
