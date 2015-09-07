cc8aca82a486aa25aae014df8b273b45febd24e3
Martti Naaber <martnaa@greeny.cs.tlu.ee>
2015-09-07 14:57:31 +0300

    login

--- user_form.php
+++ user_form.php
@@ -1,3 +1,20 @@
 <?php
 
 ?>
+
+<html>
+	<head>
+		<title>User login page</title>
+	</head>
+	<body>
+		
+		<h2>Login</h2>
+		<input type="email" placeholder="E-post"> <br><br>
+		<input type="password" placeholder="Parool"> <br><br>
+		
+		<input type="submit" value="Logi sisse">
+		
+		<h2>Create user</h2>
+		
+	</body>
+</html>
